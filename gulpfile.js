70551f90d2dfd52fc66c4955af466a58344337fa
Lintra12 <uko.don@gmail.com>
2024-06-14 10:18:24 +0300

    mailer

--- gulpfile.js
+++ gulpfile.js
@@ -54,9 +54,7 @@ function watching() {
 function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
-    'app/js/main.js'
-    
-  ])
+    'app/js/main.js'])
   .pipe(concat('main.min.js'))
   .pipe(uglify())
   .pipe(dest('app/js'))
